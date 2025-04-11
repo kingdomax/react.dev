261b479fae201f05ba1887b9bb45ca742c4bf27d
kingdomax <kingdomax@windowslive.com>
2025-04-11 15:13:58 +0200

    fix error

--- src/Game.js
+++ src/Game.js
@@ -1,81 +1,67 @@
 import { useState } from "react";
 import { Board } from "./component/Board";
+import { calculateWinner } from "./service/calculateWinner";
 
 export const Game = () => {
-	const [currentMove, setCurrentMove] = useState(0);
-	const [history, setHistory] = useState([Array(9).fill(null)]);
-	const [sortByAsc, setSortByAsc] = useState(true);
-	const xIsNext = currentMove % 2 === 0;
-	const currentSquares = history[currentMove];
-	const winnerInfo = calculateWinner(currentSquares);
+    const [currentMove, setCurrentMove] = useState(0);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [sortByAsc, setSortByAsc] = useState(true);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+    const winnerInfo = calculateWinner(currentSquares);
 
-	const setStatusText = () => {
-		if (winnerInfo){ return "Winner: " + winnerInfo.name; }
-		else if (currentMove != 9) { return "Next Player: " + (xIsNext ? "X" : "O"); }
-		else { return "Draw"; }
-	};
+    const setStatusText = () => {
+        if (winnerInfo) {
+            return "Winner: " + winnerInfo.name;
+        } else if (currentMove != 9) {
+            return "Next Player: " + (xIsNext ? "X" : "O");
+        } else {
+            return "Draw";
+        }
+    };
 
-	const handlePlay = (nextSquares) => {
-		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-		setHistory(nextHistory);
-		setCurrentMove(nextHistory.length - 1);
-	};
+    // TODO: Display the location for each move in the format (row, col) in the move history list.
+    const handlePlay = (nextSquares) => {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    };
 
-	const moveList = () => {
-		const sortedHistory = sortByAsc ? history : [...history].reverse(); // Reverse if descending
-		return sortedHistory.map((_, index) => {
-		  const move = sortByAsc ? index : history.length - 1 - index; // Correct move numbering
-		  const description = move > 0 ? `Go to move #${move}` : "Go to game start";
-		  return (
-			<li key={move}>
-			  {currentMove !== move ? (
-				<button onClick={() => setCurrentMove(move)}>{description}</button>
-			  ) : (
-				<span>You are at move #{move}</span>
-			  )}
-			</li>
-		  );
-		});
-	};
+    const moveList = () => {
+        const sortedHistory = sortByAsc ? history : [...history].reverse(); // Reverse if descending
+        return sortedHistory.map((_, index) => {
+            const move = sortByAsc ? index : history.length - 1 - index; // Correct move numbering
+            const description =
+                move > 0 ? `Go to move #${move}` : "Go to game start";
+            return (
+                <li key={move}>
+                    {currentMove !== move ? (
+                        <button onClick={() => setCurrentMove(move)}>
+                            {description}
+                        </button>
+                    ) : (
+                        <span>You are at move #{move}</span>
+                    )}
+                </li>
+            );
+        });
+    };
 
-	return (
+    return (
         <div className="game">
-          <div className="game-board">
-            <Board
-				status={setStatusText()}
-				xIsNext={xIsNext}
-				winner={winnerInfo}
-				squares={currentSquares}
-				onPlay={handlePlay}
-			/>
-          </div>
-          <div className="game-info">
-			<button onClick={() => setSortByAsc(!sortByAsc)}>sort</button>
-            <ol reversed={!sortByAsc}>{moveList()}</ol>
-          </div>
+            <div className="game-board">
+                <Board
+                    status={setStatusText()}
+                    xIsNext={xIsNext}
+                    winner={winnerInfo}
+                    squares={currentSquares}
+                    onPlay={handlePlay}
+                />
+            </div>
+            <div className="game-info">
+                <button onClick={() => setSortByAsc(!sortByAsc)}>sort</button>
+                <ol reversed={!sortByAsc}>{moveList()}</ol>
+            </div>
         </div>
-	);
-};
-
-const calculateWinner = (squares) => {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6]
-	];
-	for (let i = 0; i < lines.length; i++) {
-	  	const [a, b, c] = lines[i];
-	  	if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return {
-				name: squares[a],
-				winnerIndex: lines[i],
-			};
-	  	}
-	}
-	return null;
+    );
 };
