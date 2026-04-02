66924a4319832f729aa48055a775a429ef60b79e
kingdomax <kingdomax@windowslive.com>
2026-04-02 14:25:41 +0700

    css trick

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const ReactPlaygroundTwo = lazy(
         new Promise((resolve) =>
             setTimeout(
                 () => resolve(import("./component/SetIntervalCounter")),
-                5000
-            )
-        )
+                5000,
+            ),
+        ),
 );
 
 const root = createRoot(document.getElementById("root"));
@@ -25,6 +25,38 @@ root.render(
         <Suspense fallback={<div>Lazy Loading Component.....</div>}>
             <ReactPlaygroundTwo />
         </Suspense>
-    </AppContextProvider>
+        <div
+            style={{
+                width: "100px",
+                height: "100px",
+                boxSizing: "border-box",
+                border: "1px solid black",
+            }}
+        >
+            <div
+                style={{
+                    width: "80px",
+                    height: "80px",
+                    border: "1px solid blue",
+                    margin: "10px",
+                }}
+            >
+                Content
+            </div>
+        </div>
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "12px",
+                background: "dodgerblue",
+                padding: "12px",
+            }}
+        >
+            <button></button>
+            <code style={{ color: "#fff" }}>Title</code>
+            <button style={{ marginLeft: "auto" }}>Button</button>
+        </div>
+    </AppContextProvider>,
     //</StrictMode>
 );
